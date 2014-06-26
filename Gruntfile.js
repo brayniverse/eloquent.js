4ccd82368e8703e72a910f920a39cf5488badb56
Christopher L Bray <chris@christopherbray.co.uk>
2014-06-26 07:50:37 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,42 @@
-module.exports = function(grunt) {
-  'use strict';
-
-  require('load-grunt-tasks')(grunt);
+module.exports = function( grunt ) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    
+    pkg: grunt.file.readJSON( 'package.json' ),
+    
     watch: {},
+    
     jshint: {},
+    
     concat: {
       dist: {
-        src: ['src/shema.js'],
+        src: [ 'src/shema.js' ],
         dest: 'build/eloquent.js',
       }
     },
+    
     uglify: {
       options: {
         report: 'min',
         preserveComments: 'some'
       },
+      
       dist: {
         files: {
-          'build/eloquent.min.js': ['src/schema.js'],
+          'build/eloquent.min.js': [ 'src/schema.js' ],
         }
       }
     }
   });
 
-  grunt.registerTask('default', ['concat', 'uglify']);
-  grunt.registerTask('release', ['jshint', 'concat', 'uglify', 'zip']);
+  require( 'load-grunt-tasks' )( grunt );
+
+  // Default Task
+  // Used during development, this task will watch for file changes and generate a new `build` file.
+  grunt.registerTask( 'default', [ 'watch', 'concat', 'uglify' ] );
+  
+  // Deploy Task
+  // This will build 
+  grunt.registerTask( 'deploy', [ 'jshint', 'concat', 'uglify', 'zip' ] );
+
 };
